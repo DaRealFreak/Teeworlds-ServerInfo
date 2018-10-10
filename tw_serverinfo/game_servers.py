a89286989fdb5a0af1ba680f42e1c9ddf29d6e2a
Steffen Keuper <steffen.keuper@web.de>
2018-10-10 12:12:32 +0200

    [CLEANUP] extract method for parsing the different server responses, prevent override of game server token

--- tw_serverinfo/game_servers.py
+++ tw_serverinfo/game_servers.py
@@ -28,11 +28,12 @@ class GameServers(object):
         i = 0
         for key, game_server in self._game_servers.items():
             i += 1
-            self._game_servers[key] = Network.send_packet(sock=sock, data=Network.PACKETS['SERVERBROWSE_GETINFO'],
-                                                          server=game_server)
-            self._game_servers[key] = Network.send_packet(sock=sock,
-                                                          data=Network.PACKETS['SERVERBROWSE_GETINFO_64_LEGACY'],
-                                                          server=game_server)
+            game_server = Network.send_packet(sock=sock, data=Network.PACKETS['SERVERBROWSE_GETINFO'],
+                                              server=game_server)
+            game_server = Network.send_packet(sock=sock, data=Network.PACKETS['SERVERBROWSE_GETINFO_64_LEGACY'],
+                                              server=game_server)
+            # update game server which got extended by the token
+            self._game_servers[key] = game_server
 
             if i % self.SERVER_CHUNK_SIZE or i >= len(self._game_servers):
                 duration_without_response = Network.CONNECTION_SLEEP_DURATION
@@ -64,131 +65,173 @@ class GameServers(object):
         :return:
         """
         game_server_key = '{ip:s}:{port:d}'.format(ip=server['ip'], port=server['port'])
+        server = self._game_servers[game_server_key]
+
         slots = deque(data[14:].split(b"\x00"))
         token = int(slots.popleft().decode('utf-8'))
         # ToDo: token validation
 
         if data[6:6 + 8] == Network.PACKETS['SERVERBROWSE_INFO']:
-            version = slots.popleft().decode('utf-8')
-            name = slots.popleft().decode('utf-8')
-            map_name = slots.popleft().decode('utf-8')
-            game_type = slots.popleft().decode('utf-8')
-            flags = int(slots.popleft().decode('utf-8'))
-            num_players = int(slots.popleft().decode('utf-8'))
-            max_players = int(slots.popleft().decode('utf-8'))
-            num_clients = int(slots.popleft().decode('utf-8'))
-            max_clients = int(slots.popleft().decode('utf-8'))
-
-            players = self._game_servers[game_server_key]['players']
-            while len(slots) >= 5:
-                # no idea what this is, always empty
-                players.append({
-                    'name': slots.popleft(),
-                    'clan': slots.popleft(),
-                    'country': int(slots.popleft().decode('utf-8')),
-                    'score': int(slots.popleft().decode('utf-8')),
-                    'ingame': int(slots.popleft().decode('utf-8'))
-                })
-
-            self._game_servers[game_server_key] = {
-                'ip': server['ip'],
-                'port': server['port'],
-                'type': server['type'],
-                'server_type': 'vanilla',
-                'version': version,
-                'name': name,
-                'map_name': map_name,
-                'game_type': game_type,
-                'flags': flags,
-                'num_players': num_players,
-                'max_players': max_players,
-                'num_clients': num_clients,
-                'max_clients': max_clients,
-                'players': players
-            }
+            # vanilla
+            updated_server = self.parse_vanilla_response(slots, server)
+            self._game_servers[game_server_key] = updated_server
         elif data[6:6 + 8] == Network.PACKETS['SERVERBROWSE_INFO_64_LEGACY']:
-            version = slots.popleft().decode('utf-8')
-            name = slots.popleft().decode('utf-8')
-            map_name = slots.popleft().decode('utf-8')
-            game_type = slots.popleft().decode('utf-8')
-            flags = int(slots.popleft().decode('utf-8'))
-            num_players = int(slots.popleft().decode('utf-8'))
-            max_players = int(slots.popleft().decode('utf-8'))
-            num_clients = int(slots.popleft().decode('utf-8'))
-            max_clients = int(slots.popleft().decode('utf-8'))
-
-            players = self._game_servers[game_server_key]['players']
-            if slots[0] == b'':
-                slots.popleft()
-
-            while len(slots) >= 5:
-                # no idea what this is, always empty
-                players.append({
-                    'name': slots.popleft(),
-                    'clan': slots.popleft(),
-                    'country': int(slots.popleft().decode('utf-8')),
-                    'score': int(slots.popleft().decode('utf-8')),
-                    'ingame': int(slots.popleft().decode('utf-8'))
-                })
-
-            self._game_servers[game_server_key] = {
-                'ip': server['ip'],
-                'port': server['port'],
-                'type': server['type'],
-                'server_type': '64_legacy',
-                'version': version,
-                'name': name,
-                'map_name': map_name,
-                'game_type': game_type,
-                'flags': flags,
-                'num_players': num_players,
-                'max_players': max_players,
-                'num_clients': num_clients,
-                'max_clients': max_clients,
-                'players': players
-            }
+            # 64 legacy
+            updated_server = self.parse_64_legacy_response(slots, server)
+            self._game_servers[game_server_key] = updated_server
         elif data[6:6 + 8] == Network.PACKETS['SERVERBROWSE_INFO_EXTENDED']:
-            version = slots.popleft().decode('utf-8')
-            name = slots.popleft().decode('utf-8')
-            map_name = slots.popleft().decode('utf-8')
-            map_crc = int(slots.popleft().decode('utf-8'))
-            map_size = int(slots.popleft().decode('utf-8'))
-            game_type = slots.popleft().decode('utf-8')
-            flags = int(slots.popleft().decode('utf-8'))
-            num_players = int(slots.popleft().decode('utf-8'))
-            max_players = int(slots.popleft().decode('utf-8'))
-            num_clients = int(slots.popleft().decode('utf-8'))
-            max_clients = int(slots.popleft().decode('utf-8'))
-
-            players = self._game_servers[game_server_key]['players']
-            while len(slots) >= 6:
-                # no idea what this is, always empty
-                slots.popleft()
-                players.append({
-                    'name': slots.popleft(),
-                    'clan': slots.popleft(),
-                    'country': int(slots.popleft().decode('utf-8')),
-                    'score': int(slots.popleft().decode('utf-8')),
-                    'ingame': int(slots.popleft().decode('utf-8'))
-                })
-
-            self._game_servers[game_server_key] = {
-                'ip': server['ip'],
-                'port': server['port'],
-                'type': server['type'],
-                'server_type': 'ext',
-                'version': version,
-                'name': name,
-                'map_name': map_name,
-                'map_crc': map_crc,
-                'map_size': map_size,
-                'game_type': game_type,
-                'flags': flags,
-                'num_players': num_players,
-                'max_players': max_players,
-                'num_clients': num_clients,
-                'max_clients': max_clients,
-                'players': players
-            }
+            # extended response, current default of DDNet
+            updated_server = self.parse_extended_response(slots, server)
+            self._game_servers[game_server_key] = updated_server
         elif data[6:6 + 8] == Network.PACKETS['SERVERBROWSE_INFO_EXTENDED_MORE']:
             print('no idea what to expect here, never got useful data')
+
+    @staticmethod
+    def parse_vanilla_response(slots: deque, server: dict) -> dict:
+        """Parse the default response of the vanilla client
+
+        :param slots:
+        :param server:
+        :return:
+        """
+        version = slots.popleft().decode('utf-8')
+        name = slots.popleft().decode('utf-8')
+        map_name = slots.popleft().decode('utf-8')
+        game_type = slots.popleft().decode('utf-8')
+        flags = int(slots.popleft().decode('utf-8'))
+        num_players = int(slots.popleft().decode('utf-8'))
+        max_players = int(slots.popleft().decode('utf-8'))
+        num_clients = int(slots.popleft().decode('utf-8'))
+        max_clients = int(slots.popleft().decode('utf-8'))
+
+        players = server['players']
+        while len(slots) >= 5:
+            # no idea what this is, always empty
+            players.append({
+                'name': slots.popleft(),
+                'clan': slots.popleft(),
+                'country': int(slots.popleft().decode('utf-8')),
+                'score': int(slots.popleft().decode('utf-8')),
+                'ingame': int(slots.popleft().decode('utf-8'))
+            })
+
+        return {
+            'ip': server['ip'],
+            'port': server['port'],
+            'type': server['type'],
+            'server_type': 'vanilla',
+            'version': version,
+            'name': name,
+            'map_name': map_name,
+            'game_type': game_type,
+            'flags': flags,
+            'num_players': num_players,
+            'max_players': max_players,
+            'num_clients': num_clients,
+            'max_clients': max_clients,
+            'players': players
+        }
+
+    @staticmethod
+    def parse_64_legacy_response(slots: deque, server: dict) -> dict:
+        """Parse the 64 slot legacy response
+
+        :param slots:
+        :param server:
+        :return:
+        """
+        version = slots.popleft().decode('utf-8')
+        name = slots.popleft().decode('utf-8')
+        map_name = slots.popleft().decode('utf-8')
+        game_type = slots.popleft().decode('utf-8')
+        flags = int(slots.popleft().decode('utf-8'))
+        num_players = int(slots.popleft().decode('utf-8'))
+        max_players = int(slots.popleft().decode('utf-8'))
+        num_clients = int(slots.popleft().decode('utf-8'))
+        max_clients = int(slots.popleft().decode('utf-8'))
+
+        players = server['players']
+        if slots[0] == b'':
+            # no idea what this is, always empty
+            slots.popleft()
+
+        while len(slots) >= 5:
+            player = {
+                'name': slots.popleft(),
+                'clan': slots.popleft(),
+                'country': int(slots.popleft().decode('utf-8')),
+                'score': int(slots.popleft().decode('utf-8')),
+                'ingame': int(slots.popleft().decode('utf-8'))
+            }
+            if player not in players:
+                players.append(player)
+
+        return {
+            'ip': server['ip'],
+            'port': server['port'],
+            'type': server['type'],
+            'server_type': '64_legacy',
+            'version': version,
+            'name': name,
+            'map_name': map_name,
+            'game_type': game_type,
+            'flags': flags,
+            'num_players': num_players,
+            'max_players': max_players,
+            'num_clients': num_clients,
+            'max_clients': max_clients,
+            'players': players
+        }
+
+    @staticmethod
+    def parse_extended_response(slots: deque, server: dict) -> dict:
+        """Parse the extended server info response(default for DDNet)
+
+        :param slots:
+        :param server:
+        :return:
+        """
+        version = slots.popleft().decode('utf-8')
+        name = slots.popleft().decode('utf-8')
+        map_name = slots.popleft().decode('utf-8')
+        map_crc = int(slots.popleft().decode('utf-8'))
+        map_size = int(slots.popleft().decode('utf-8'))
+        game_type = slots.popleft().decode('utf-8')
+        flags = int(slots.popleft().decode('utf-8'))
+        num_players = int(slots.popleft().decode('utf-8'))
+        max_players = int(slots.popleft().decode('utf-8'))
+        num_clients = int(slots.popleft().decode('utf-8'))
+        max_clients = int(slots.popleft().decode('utf-8'))
+
+        players = server['players']
+        while len(slots) >= 6:
+            # no idea what this is, always empty
+            slots.popleft()
+            player = {
+                'name': slots.popleft(),
+                'clan': slots.popleft(),
+                'country': int(slots.popleft().decode('utf-8')),
+                'score': int(slots.popleft().decode('utf-8')),
+                'ingame': int(slots.popleft().decode('utf-8'))
+            }
+            if player not in players:
+                players.append(player)
+
+        return {
+            'ip': server['ip'],
+            'port': server['port'],
+            'type': server['type'],
+            'server_type': 'ext',
+            'version': version,
+            'name': name,
+            'map_name': map_name,
+            'map_crc': map_crc,
+            'map_size': map_size,
+            'game_type': game_type,
+            'flags': flags,
+            'num_players': num_players,
+            'max_players': max_players,
+            'num_clients': num_clients,
+            'max_clients': max_clients,
+            'players': players
+        }
