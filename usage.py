a78032025994ec0042b6f2559222ee5b72a265b8
Steffen Keuper <steffen.keuper@web.de>
2018-10-10 04:55:32 +0200

    [TASK] update usage for manual case since master servers are currently down

--- usage.py
+++ usage.py
@@ -5,7 +5,20 @@
 from pprint import pprint
 
 from tw_serverinfo import MasterServers
+from tw_serverinfo import GameServers
 
 if __name__ == '__main__':
     m = MasterServers()
-    pprint(m.game_servers)
+    g = GameServers()
+    # pprint(m.game_servers)
+    # ddnet example: 95.172.92.151:8334
+    pprint(g.fill_server_info(
+        {
+            '95.172.92.151:8334': {
+                'ip': '95.172.92.151',
+                'port': 8334,
+                'type': 'game',
+                'players': []
+            }
+        }
+    ))
