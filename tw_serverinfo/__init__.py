c7c9ae97f3ca1aa5cacffbbdab9e49ee4dff734b
Steffen Keuper <steffen.keuper@web.de>
2018-10-10 04:54:48 +0200

    [TASK] add GameServers class to __all__

--- tw_serverinfo/__init__.py
+++ tw_serverinfo/__init__.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
-from tw_serverinfo.network import Network
+from tw_serverinfo.game_servers import GameServers
 from tw_serverinfo.master_servers import MasterServers
+from tw_serverinfo.network import Network
 
-__all__ = [Network, MasterServers]
+__all__ = [Network, MasterServers, GameServers]
