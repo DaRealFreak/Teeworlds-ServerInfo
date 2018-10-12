d36816af3c928ea3e696217d8140a259856ab71f
Steffen Keuper <steffen.keuper@web.de>
2018-10-12 12:42:25 +0200

    [TASK] update example usage to new models too

--- usage.py
+++ usage.py
@@ -4,21 +4,17 @@
 # This is an example usage
 from pprint import pprint
 
-from tw_serverinfo import MasterServers
 from tw_serverinfo import GameServers
+from tw_serverinfo import MasterServers
+from tw_serverinfo.models.game_server import GameServer
 
 if __name__ == '__main__':
     m = MasterServers()
     g = GameServers()
-    # pprint(m.game_servers)
-    # ddnet example: 95.172.92.151:8334
-    pprint(g.fill_server_info(
-        {
-            '95.172.92.151:8334': {
-                'ip': '95.172.92.151',
-                'port': 8334,
-                'type': 'game',
-                'players': []
-            }
-        }
-    ))
+    # master server example
+    pprint(m.master_servers)
+    # game server example using DDNet server
+    # DDNet example: 95.172.92.151:8303
+    game_server = GameServer(ip='95.172.92.151', port=8303)
+    g.fill_server_info([game_server])
+    pprint(game_server)
