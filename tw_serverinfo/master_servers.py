769f03e2fc9f5d6a1225714c7c00f87fe7f7866d
Steffen Keuper <steffen.keuper@web.de>
2018-10-13 22:29:58 +0200

    [TASK] return only the servers from master_servers, add extra arguments for send_packet function, add function to retrieve all game servers, prevent master server from adding itsself to game servers

--- tw_serverinfo/master_servers.py
+++ tw_serverinfo/master_servers.py
@@ -47,7 +47,7 @@ class MasterServers(object):
         return self._master_servers
 
     @property
-    def master_servers(self) -> dict:
+    def master_servers(self) -> list:
         """Returns the game servers and cache the result
 
         :return:
@@ -57,7 +57,20 @@ class MasterServers(object):
             # clear the cache if the last index was more than 10 minutes ago
             if time() >= self.update_master_server_info()['timestamp'] + 60 * 10:
                 self.update_master_server_info.cache_clear()
-        return self.update_master_server_info()
+        return self.update_master_server_info()['servers']
+
+    @property
+    def game_servers(self) -> list:
+        """Returns the game servers from the updated master server results
+
+        :return:
+        """
+        game_servers = []
+        for master_server in self.master_servers:  # type: MasterServer
+            for game_server in master_server.servers:  # type: GameServer
+                if game_server not in game_servers:
+                    game_servers.append(game_server)
+        return game_servers
 
     @lru_cache(maxsize=None)
     def update_master_server_info(self) -> dict:
@@ -71,26 +84,30 @@ class MasterServers(object):
         sock.setblocking(False)
 
         for master_server in self.get_master_servers():
-            Network.send_packet(sock=sock, data=Network.PACKETS['SERVERBROWSE_GETCOUNT'], server=master_server)
-            Network.send_packet(sock=sock, data=Network.PACKETS['SERVERBROWSE_GETLIST'], server=master_server)
-            # update master server entry which probably got modified
+            Network.send_packet(sock=sock, data=Network.PACKETS['SERVERBROWSE_GETCOUNT'], extra_data=b'\xff\xff',
+                                server=master_server, add_token=False)
+            Network.send_packet(sock=sock, data=Network.PACKETS['SERVERBROWSE_GETLIST'], extra_data=b'\xff\xff',
+                                server=master_server, add_token=False)
+            # apparently since 06.10.2018 we need the get info packet for the master servers too to retrieve the
+            # game servers, normally sent after parsing the response from the previous 2 packets, but works
+            # as well if sent directly since no information of the response for the previous 2 packets is needed
+            Network.send_packet(sock=sock, data=Network.PACKETS['SERVERBROWSE_GETINFO'], extra_data=b'xe',
+                                server=master_server)
 
         duration_without_response = Network.CONNECTION_SLEEP_DURATION
         sleep(Network.CONNECTION_SLEEP_DURATION / 1000.0)
 
-        while True:
+        while duration_without_response < Network.CONNECTION_TIMEOUT:
             if not Network.receive_packet(sock, self.get_master_servers(), self.process_packet):
-                if duration_without_response > Network.CONNECTION_TIMEOUT:
-                    # we didn't receive any packets in time and cancel the connection here
-                    sock.close()
-                    break
-                else:
-                    # increase the measured duration without a response and sleep for the set duration
-                    duration_without_response += Network.CONNECTION_SLEEP_DURATION
-                    sleep(Network.CONNECTION_SLEEP_DURATION / 1000.0)
+                # increase the measured duration without a response and sleep for the set duration
+                duration_without_response += Network.CONNECTION_SLEEP_DURATION
+                sleep(Network.CONNECTION_SLEEP_DURATION / 1000.0)
             else:
                 # if we got a response reset the duration in case we receive multiple packets
                 duration_without_response = 0
+
+        # we didn't receive any packets in time and cancel the connection here
+        sock.close()
 
         return {
             'servers': self._master_servers,
@@ -98,20 +115,20 @@ class MasterServers(object):
         }
 
     @staticmethod
-    def process_packet(data: bytes, server: MasterServer) -> None:
+    def process_packet(data: bytes, master_server: MasterServer) -> None:
         """Process packet function for
          - SERVERBROWSE_COUNT
          - SERVERBROWSE_LIST
         packets
 
         :type data: bytes
-        :type server: dict
+        :type master_server: dict
         :return:
         """
-        server.response = True
+        master_server.response = True
 
         if data[6:6 + 8] == Network.PACKETS['SERVERBROWSE_COUNT']:
-            server.num_servers = (data[14] << 8) | data[15]
+            master_server.num_servers = (data[14] << 8) | data[15]
         elif data[6:6 + 8] == Network.PACKETS['SERVERBROWSE_LIST']:
             for i in range(14, len(data) - 14, 18):
                 if data[i:i + 12] == b'\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\xff\xff':
@@ -121,5 +138,6 @@ class MasterServers(object):
 
                 port = int.from_bytes(data[i + 16:i + 18], byteorder='big')
 
-                game_server = GameServer(ip=ip, port=port)
-                server.servers.append(game_server)
+                if ip != master_server.ip or port != master_server.port:
+                    game_server = GameServer(ip=ip, port=port)
+                    master_server.servers.append(game_server)
