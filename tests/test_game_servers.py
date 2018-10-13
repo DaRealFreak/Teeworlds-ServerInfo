6a14d4e3e6b46aab7587eecdfab3c5c167716aeb
Steffen Keuper <steffen.keuper@web.de>
2018-10-13 22:31:52 +0200

    [TASK] check all game servers in the unit test instead of only one

--- tests/test_game_servers.py
+++ tests/test_game_servers.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 import unittest
 
-from tw_serverinfo import GameServers
+from tw_serverinfo import GameServers, MasterServers
 from tw_serverinfo.models.game_server import GameServer
 
 
@@ -13,8 +13,9 @@ class TestGameServers(unittest.TestCase):
 
         :return:
         """
+        master_servers_module = MasterServers()
         self.game_servers_module = GameServers()
-        self.game_server = GameServer(ip='95.172.92.151', port=8303)
+        self.game_servers = master_servers_module.game_servers
 
     def test_fill_server_info(self):
         """Test the fill_server_info function to retrieve and parse data from the the game servers
@@ -22,14 +23,15 @@ class TestGameServers(unittest.TestCase):
 
         :return:
         """
-        self.game_servers_module.fill_server_info([self.game_server])
-        if self.game_server.response:
-            self.assertTrue(self.game_server.name != '')
-            self.assertTrue(self.game_server.max_players > 0)
-            self.assertTrue(self.game_server.max_clients > 0)
-            self.assertTrue(self.game_server.token != b'')
-            self.assertTrue(self.game_server.game_type != '')
-            self.assertTrue(self.game_server.map_name != '')
+        self.game_servers_module.fill_server_info(self.game_servers)
+        for game_server in self.game_servers:  # type: GameServer
+            if game_server.response:
+                self.assertTrue(game_server.name != '')
+                self.assertTrue(game_server.max_players > 0)
+                self.assertTrue(game_server.max_clients > 0)
+                self.assertTrue(game_server.token != b'')
+                self.assertTrue(game_server.game_type != '')
+                self.assertTrue(game_server.map_name != '')
 
 
 if __name__ == '__main__':
