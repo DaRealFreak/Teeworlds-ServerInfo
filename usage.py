297aa98f52497b3ab69c1afd7bc4b92024554e14
Steffen Keuper <steffen.keuper@web.de>
2018-10-13 22:31:32 +0200

    [TASK] use new game_servers function to fill information for all game servers

--- usage.py
+++ usage.py
@@ -6,15 +6,14 @@ from pprint import pprint
 
 from tw_serverinfo import GameServers
 from tw_serverinfo import MasterServers
-from tw_serverinfo.models.game_server import GameServer
 
 if __name__ == '__main__':
     m = MasterServers()
     g = GameServers()
     # master server example
     pprint(m.master_servers)
-    # game server example using DDNet server
-    # DDNet example: 95.172.92.151:8303
-    game_server = GameServer(ip='95.172.92.151', port=8303)
-    g.fill_server_info([game_server])
-    pprint(game_server)
+    print('amount of game servers: {amount:d}'.format(amount=len(m.game_servers)))
+    # request the server info for the game servers
+    g.fill_server_info(m.game_servers)
+    for game_server in m.game_servers:
+        pprint(game_server)
