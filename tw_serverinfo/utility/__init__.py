1f441541be78b53f0bf61d007b3b3d5e3c54ffb8
Steffen Keuper <steffen.keuper@web.de>
2018-10-14 02:50:26 +0200

    [TASK] add custom teeworlds country codes to utilities

---
+++ tw_serverinfo/utility/__init__.py
@@ -0,0 +1,5 @@
+#!/usr/local/bin/python
+# coding: utf-8
+from tw_serverinfo.utility.countries import custom_countries
+
+__all__ = [custom_countries]
---
+++ tw_serverinfo/utility/countries.py
@@ -0,0 +1,37 @@
+#!/usr/local/bin/python
+# coding: utf-8
+
+custom_countries = {
+    '-1': {
+        'code': 'default',
+        'name': 'none'
+    },
+    '901': {
+        'code': 'XEN',
+        'name': 'England'
+    },
+    '902': {
+        'code': 'XN1',
+        'name': 'Northern Ireland'
+    },
+    '903': {
+        'code': 'XSC',
+        'name': 'Scotland'
+    },
+    '904': {
+        'code': 'XWA',
+        'name': 'Wales'
+    },
+    '905': {
+        'code': 'XEU',
+        'name': 'Europe'
+    },
+    '906': {
+        'code': 'CT',
+        'name': 'Catalan'
+    },
+    '737': {
+        'code': 'SS',
+        'name': 'South Sudan'
+    },
+}
