062d8e7c16d85b75520b12de1155e937714088a7
DaRealFreak <steffen.keuper@web.de>
2018-10-29 21:58:12 +0100

    [DOC] update module description and add classifiers and long description to setup.py

--- tw_serverinfo/__version__.py
+++ tw_serverinfo/__version__.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 
 __title__ = 'Teeworlds Server Info'
-__description__ = 'Small module to retrieve game servers and player information'
+__description__ = 'Small module to retrieve game servers and player information for the game Teeworlds'
 __version__ = '1.0.0'
 __url__ = 'https://github.com/DaRealFreak/Teeworlds-ServerInfo'
 __author__ = 'DaRealFreak'
