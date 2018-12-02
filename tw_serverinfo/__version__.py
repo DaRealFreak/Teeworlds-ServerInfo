62441fc62a2a69ddc8ad38cab00e20342ced7637
DaRealFreak <steffen.keuper@web.de>
2018-12-02 18:03:28 +0100

    [TASK] bump version to 1.0.1

--- tw_serverinfo/__version__.py
+++ tw_serverinfo/__version__.py
@@ -3,7 +3,7 @@
 
 __title__ = 'Teeworlds Server Info'
 __description__ = 'Small module to retrieve game servers and player information for the game Teeworlds'
-__version__ = '1.0.0'
+__version__ = '1.0.1'
 __url__ = 'https://github.com/DaRealFreak/Teeworlds-ServerInfo'
 __author__ = 'DaRealFreak'
 __author_email__ = 'steffen.keuper@web.de'
